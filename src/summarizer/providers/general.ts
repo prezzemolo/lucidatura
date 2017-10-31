a74fb5dbcfb19d73ceb7151727f7f40bb78ce756
prezzemolo <git@prezzemolo.ga>
2017-10-31 14:52:14 +0900

    :octocat: summarizer - add GitHub provider
    * you can see supported endpoints at Map named 'summarizers'.

--- src/summarizer/providers/general.ts
+++ src/summarizer/providers/general.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as qs from 'query-string'
 
 import { ISummary } from '../../interfaces'
-import { StatusCodeError } from '../../errors'
+import { commonAxiosErrorHandler } from './common'
 
 export default (url: string, lang: string, ref: string = 'refs/tags/0.4.5'): Promise<ISummary> =>
   axios
@@ -10,19 +10,12 @@ export default (url: string, lang: string, ref: string = 'refs/tags/0.4.5'): Pro
     .then(response => {
       return response.data as ISummary
     })
-    .catch(reason => {
-      if (reason.response) {
-        if (typeof reason.response.data === 'string') {
-          throw new StatusCodeError(reason.response.status, reason.response.data)
-        }
-
-        throw new StatusCodeError(reason.response.status, `${
-          // cut a suffix '.'
-          reason.response.data.title.endsWith('.')
-            ? reason.response.data.title.slice(0, -1)
-            : reason.response.data.title
-        }, ${reason.response.data.detail.toLocaleLowerCase()}`)
-      }
-
-      throw reason
-    })
+    .catch(commonAxiosErrorHandler(data => {
+      if (typeof data !== 'object') return null
+      return `${
+        // cut a suffix '.'
+        data.title.endsWith('.')
+          ? data.title.slice(0, -1)
+          : data.title
+      }, ${data.detail.toLocaleLowerCase()}`
+    }))
