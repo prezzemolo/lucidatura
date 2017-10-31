3c7b4dddf21d21ff77877c54044a00fb4c7a59d6
prezzemolo <git@prezzemolo.ga>
2017-10-31 14:55:34 +0900

    :paperclip: test - check whether to select 'github' provider from GitHub URL

--- tests/summarizer.ts
+++ tests/summarizer.ts
@@ -1,12 +1,18 @@
 import test from 'ava'
 
 import generalProvider from '../src/summarizer/providers/general'
+import githubProvider from '../src/summarizer/providers/github'
 import summarizer, { selectProvider } from '../src/summarizer'
 import { SummarizerNotFoundError } from '../src/errors'
 
 test(`summarizer > select 'general' provider from URL 'http://example.com'`, t => {
   const provider = selectProvider('http://example.com')
   t.is(provider, generalProvider)
+})
+
+test(`summarizer > select 'github' provider from URL 'https://github.com'`, t => {
+  const provider = selectProvider('http://github.com')
+  t.is(provider, githubProvider)
 })
 
 test(`summarizer > throw SummarizerNotFoundError with unsupported URL 'ftp://example.com'`, async t => {
